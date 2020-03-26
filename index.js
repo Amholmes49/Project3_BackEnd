d28ad917ca5f4a7185b55d8b20a698ffa8a9ea06
Aaron Holmes <HolmesAaron@Johndeere.com>
2020-03-26 13:51:07 -0500

    Heroku update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const playersController = require('./controllers/CPlayers');
 const teamsController = require('./controllers/CTeams');
-const venuesController = require('./controllers/CVenues');
+// const venuesController = require('./controllers/CVenues');
 const cors = require('cors')
 
 //Middleware
@@ -12,7 +12,7 @@ app.use(express.json());
 //Controllers
 app.use('/api/Players', playersController);
 app.use('/api/Teams', teamsController);
-app.use('/api/Venues', venuesController);
+// app.use('/api/Venues', venuesController);
 
 app.set("port", process.env.PORT || 8080);
 
